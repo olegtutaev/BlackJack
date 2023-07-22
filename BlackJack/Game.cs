9cdf268362574ea515666be0be654d2525faa8f3
olegtutaev <oleg.tutaeff@yandex.ru>
2023-07-22 13:17:48 +0400

    Создан скилет методов
    Создан скилет методов

---
+++ BlackJack/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Game
+    {
+
+        
+
+        public void Init()
+        {
+            initCard();
+            //Инициализировать колоду.
+
+
+        }
+
+        private void initCard()
+        {
+            // заполение карты
+        }
+
+        public void PlayGame()
+        {
+            // Инициализация двух экземпляров игроков, каждому даётся сразу две карты
+        }
+
+        public void CalculateScoresUser()
+        {
+
+        }
+
+        public void CalculateScoresComputer()
+        {
+
+        }
+
+        // Создать класс Player
+
+        public void TakeOneMoreCardUser() // Хочет взять ещё пользователь или нет
+        {
+
+        }
+
+        public void TakeOneMoreCardComputer()
+        {
+
+        }
+
+        
+        
+
+
+
+    }
+}
